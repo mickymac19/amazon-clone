3109a4b04a64db238efdf7bccedec5a82a7f13fe
Michelle Macato <michellefmacato@gmail.com>
2020-09-23 16:18:08 +0900

    header completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Hello World!
-        </h1>
+      <h1></h1>
         <Header />
+        <Home />
         {/* Home */}
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 function Header() {
     return (
@@ -13,8 +15,10 @@ function Header() {
                     <input
                     className="header_searchInput"
                     type="text"/>
-                    {/* logo */}       
+                    <SearchIcon 
+                    className="header_searchIcon" />      
             </div>
+
             <div className="header_nav">
                 <div className="header_option">
                     <span className="header_optionLineOne">Hello Guest</span>
@@ -22,13 +26,17 @@ function Header() {
                 </div>
                 <div className="header_option">
                      <span className="header_optionLineOne">Returns</span>
-                     <span className="header_optionLineTwo">Orders</span>
+                     <span className="header_optionLineTwo">& Orders</span>
                 </div>
                 <div className="header_option">
                     <span className="header_optionLineOne">Your</span>
-                    <span className="header_optionLineOne">Prime</span>
+                    <span className="header_optionLineTwo">Prime</span>
                 </div>
-
+                <div className="header_optionBasket">
+                    <ShoppingBasketIcon /> 
+                    <span className="header_optionLineTwo 
+                    header_basketCount">0</span>
+                </div>
             </div>
             
         </div>
