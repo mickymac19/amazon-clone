3ad6a35f35dba10f163141cc5638f61d61d171c8
Michelle Macato <michellefmacato@gmail.com>
2020-09-23 11:08:32 +0900

    initial setup for amazon-clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDc5KZipD5dveB5w3tfHUKh7OZDU1ppgyY",
+    authDomain: "clone-540b7.firebaseapp.com",
+    databaseURL: "https://clone-540b7.firebaseio.com",
+    projectId: "clone-540b7",
+    storageBucket: "clone-540b7.appspot.com",
+    messagingSenderId: "540241664718",
+    appId: "1:540241664718:web:bcfe194c623c8661cb0e9f",
+    measurementId: "G-GPRPK06GMN"
+  };
