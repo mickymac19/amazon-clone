aea124591b8d9bdc3888c8b8ebba202d1ab75817
Michelle Macato <michellefmacato@gmail.com>
2020-09-24 16:38:10 +0900

    added styling to subtotal and added StateProvider

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -5,7 +5,28 @@ import CurrencyFormat from "react-currency-format";
 function Subtotal() {
     return (
         <div className="subtotal">
-            
+            <CurrencyFormat
+              renderText={(value) => (
+                <>
+                 <p>
+                    {/* Homework */}
+                   Subtotal ( 0 items):
+                   <strong>0</strong>
+                 </p>
+                 <small className="subtotal__gift">
+                    <input type="checkbox" /> This order contains
+                    a gift 
+                 </small>
+                </>
+              )}
+              decimalScale={2}
+              value={0} // Homework
+              displayType={"text"}
+              thousandSeparator={true}
+              prefix={"¥"}
+              />
+
+              <button>Proceed to Checkout</button>
         </div>
     );
 }
