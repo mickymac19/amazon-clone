44e4f34d6d735711aa86d2634ef0282fc8818fc4
Michelle Macato <michellefmacato@gmail.com>
2020-09-28 15:08:57 +0900

    added the functionality of setting email and password

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { Link } from "react-router-dom";
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <div className="login">
             <Link to='/'>
@@ -11,6 +14,28 @@ function Login() {
                 src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png'
             />
             </Link>
+
+            <div className='login__container'>
+                <h1>Sign-in</h1>
+                <form>
+                    <h5>E-mail</h5>
+                    <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
+
+                    <h5>Password</h5>
+                    <input type='password' value={password} onChange={e => setPassword(e.target.value)}  />
+
+                    <button className='login__signInButton'>Sign In</button>
+                </form>
+
+                <p>
+                    By signing-in you agree to Micky's Condition
+                    of Use & Sale. Please see our Privacy Notice,
+                    our Cookies Notice and our interest-based Ads 
+                    Notice.
+                </p>
+
+                <button className='login__registerButton'>Create your Amazon Account</button>
+            </div>
         </div>
     );
 }
