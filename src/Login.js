67b0fdc5038f4f6ba4a258c1011dd0797d4d57c4
Michelle Macato <michellefmacato@gmail.com>
2020-10-01 13:58:52 +0900

    added the sigin and create an account functionality

--- src/Login.js
+++ src/Login.js
@@ -1,22 +1,40 @@
 import React, { useState } from 'react';
 import './Login.css';
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { auth } from "./firebase";
 
 function Login() {
+    const history =useHistory();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const signIn = e => {
         e.preventDefault();
 
-        // some fancy firebase login....
+        auth
+            .signInWithEmailAndPassword(email, password)
+            .then(auth => {
+                history.push('/')
+            })
+
+            .catch(error => alert(error.message))
     }
 
     const register = e => {
         e.preventDefault(); 
+
+        auth
+            .createUserWithEmailAndPassword(email,password)
+            .then((auth) => {
+
+            // it successfully created a new user with email and password
+            if (auth) {
+                history.push('/')
+            }
+        })
+        .catch(error => alert(error.message))
     }
 
-        // some fancy firebase register... 
     return (
         <div className="login">
             <Link to='/'>
