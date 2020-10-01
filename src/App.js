55418db3e4df671e6eadcbcf90afd4cd8322a0dc
Michelle Macato <michellefmacato@gmail.com>
2020-10-02 08:52:34 +0900

    added the logic for dispatch and useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect }from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
@@ -6,9 +6,37 @@ import { BrowserRouter as Router, Switch, Route }
 from "react-router-dom";
 import Checkout from "./Checkout";
 import Login from "./Login";
-
+import { auth } from "./firebase";
+import { useStateValue } from './StateProvider';
 
 function App() {
+
+  const [{}, dispatch] =useStateValue();
+
+  useEffect(() => {
+
+    auth.onAuthStateChanged(authUser => {
+      console.log('THE USER IS >>>', authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      } else {
+        // the user is logged out
+        dispatch({
+          type: 'SET_USER', 
+          user: null
+        })
+      }
+    })
+  }, [])
+
+
+
   return (
 
     <Router>
